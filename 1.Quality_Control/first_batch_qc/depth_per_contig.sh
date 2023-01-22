a9f8652ddcdf835721d67155b8d3ab17e6a9a613
Alan Bergland <aob2x@virginia.edu>
2023-01-22 17:07:51 -0500

    Update depth_per_contig.sh

--- 1.Quality_Control/first_batch_qc/depth_per_contig.sh
+++ 1.Quality_Control/first_batch_qc/depth_per_contig.sh
@@ -13,16 +13,13 @@
 #ijob -c20 --mem=20G -p standard -A berglandlab
 
 # sbatch --array=1-724 ~/DESTv2_data_paper/1.Quality_Control/first_batch_qc/depth_per_contig.sh
-# sacct -j 46190508
-# cat /scratch/aob2x/dest/slurmOutput/bam_qc.46190508_3.err
+# sacct -j 46190555
+# cat /scratch/aob2x/dest/slurmOutput/bam_qc.46190555_40.err
 
 ### define jobs
   # ls -l /project/berglandlab/DEST/dest_mapped/* | cut -d' ' -f9 > /scratch/aob2x/depth/jobs.csv
-  # head -n3 /scratch/aob2x/depth/jobs.csv
-  # sed -i '1,3d' /scratch/aob2x/depth/jobs.csv
-  # head -n1 /scratch/aob2x/depth/jobs.csv
-#
-#
+find /project/berglandlab/DEST/dest_mapped/ -name '*.original.bam' > /scratch/aob2x/depth/jobs.csv
+
   #wc -l /scratch/aob2x/depth/jobs.csv
   #SLURM_ARRAY_TASK_ID=1
 
@@ -30,23 +27,25 @@
   job=$( sed "${jobN}q;d" /scratch/aob2x/depth/jobs.csv )
   echo $job
 
+  samp=$( echo $job | rev | cut -f1 -d'/' | rev )
+  echo $samp
   # job=CH_Ors_23_Aug_2020
-  ### fo 
+  ### fo
 ### load modules
   module load samtools
 
 ### iterate
-  cp /project/berglandlab/DEST/*/*/${job}/${job}.original.bam \
-  /scratch/aob2x/depth/.
+  cp ${job} \
+  /scratch/aob2x/depth/${samp}
 
-  samtools index -@ 20 /scratch/aob2x/depth/${job}.original.bam
+  samtools index -@ 20 /scratch/aob2x/depth/${samp}
 
   ~/mosdepth.1 \
   -t 20 \
   -n \
   -x \
-  /scratch/aob2x/depth/${job} \
-  /scratch/aob2x/depth/${job}.original.bam
+  /scratch/aob2x/depth/${samp} \
+  /scratch/aob2x/depth/${samp}
 
-  rm /scratch/aob2x/depth/${job}.original.bam
-  rm /scratch/aob2x/depth/${job}.original.bam.bai
+  rm /scratch/aob2x/depth/${job}
+  rm /scratch/aob2x/depth/${job}.bai
