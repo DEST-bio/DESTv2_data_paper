c31ebe96dc5698a8dbbdc20ec895c3a80b137730
Alan Bergland <aob2x@virginia.edu>
2023-01-25 08:44:04 -0500

    Update getSmallBams.sh

--- freebayes/getSmallBams.sh
+++ freebayes/getSmallBams.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 #
-#SBATCH -J runSnakemake # A single job name for the array
+#SBATCH -J smallBam # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 01:00:00 ### 1 hours
 #SBATCH --mem 1G
-#SBATCH -o /scratch/aob2x/DESTv2_output/logs/runSnakemake.%A_%a.out # Standard output
-#SBATCH -e /scratch/aob2x/DESTv2_output/logs/runSnakemake.%A_%a.err # Standard error
+#SBATCH -o /scratch/aob2x/DESTv2_output/logs/smallBam.%A_%a.out # Standard output
+#SBATCH -e /scratch/aob2x/DESTv2_output/logs/smallBam.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab_standard
 
 module load samtools/1.9
 
-### sbatch --array=1-687 /scratch/aob2x/DESTv2_data_paper/freebayes/getSmallBams.sh
+### sbatch --array=1-687 ~/DESTv2_data_paper/freebayes/getSmallBams.sh
+# sacct -j 46215357
+# cat /scratch/aob2x/DESTv2_output/logs/runSnakemake.46215357_50.err
+
 
 # find /project/berglandlab/DEST/dest_mapped/ -name '*.mel.bam' > /scratch/aob2x/smallBam/jobs.csv
 # SLURM_ARRAY_TASK_ID=1
+
 jobN=${SLURM_ARRAY_TASK_ID}
 job=$( sed "${jobN}q;d" /scratch/aob2x/smallBam/jobs.csv )
 echo $job
+
+if [ ! -f $job.bai ]
+then
+    samtools index $job
+fi
+
 
 samp=$( echo $job | rev | cut -f1 -d'/' | rev )
 echo $samp
