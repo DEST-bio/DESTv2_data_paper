29da43c254496b3db2701d41d510a371c8efa605
capoony <capoony@gmail.com>
2023-03-31 17:29:06 +0200

    update

--- misc/PnPs/scripts/SynNonsynFromSNPeff.py
+++ misc/PnPs/scripts/SynNonsynFromSNPeff.py
@@ -2,12 +2,12 @@ import sys
 from collections import defaultdict as d
 from optparse import OptionParser, OptionGroup
 
-#Author: Martin Kapun
+# Author: Martin Kapun
 
 #########################################################   HELP   #########################################################################
-usage="python %prog --input file --output file "
+usage = "python %prog --input file --output file "
 parser = OptionParser(usage=usage)
-group=OptionGroup(parser,'< put description here >')
+group = OptionGroup(parser, '< put description here >')
 
 #########################################################   CODE   #########################################################################
 
@@ -16,26 +16,28 @@ parser.add_option("--input", dest="IN", help="Input file")
 (options, args) = parser.parse_args()
 parser.add_option_group(group)
 
+
 def load_data(x):
-  ''' import data either from a gzipped or or uncrompessed file or from STDIN'''
-  import gzip
-  if x=="-":
-      y=sys.stdin
-  elif x.endswith(".gz"):
-      y=gzip.open(x,"rt", encoding="latin-1")
-  else:
-      y=open(x,"r", encoding="latin-1")
-  return y
+    ''' import data either from a gzipped or or uncrompessed file or from STDIN'''
+    import gzip
+    if x == "-":
+        y = sys.stdin
+    elif x.endswith(".gz"):
+        y = gzip.open(x, "rt", encoding="latin-1")
+    else:
+        y = open(x, "r", encoding="latin-1")
+    return y
+
 
 for l in load_data(options.IN):
-    Type=[]
-    a=l.rstrip().split()
+    Type = []
+    a = l.rstrip().split()
     if l.startswith("#"):
         continue
     if "synonymous_variant" in a[7]:
         Type.append("Syn")
     elif "missense_variant" in a[7]:
         Type.append("Non")
-    if Type==[] or len(Type)==2:
+    if Type == [] or len(Type) == 2:
         continue
-    print("\t".join([a[0],a[1],",".join(Type)]))
+    print("\t".join([a[0], a[1], ",".join(Type)]))
