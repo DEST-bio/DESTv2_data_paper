b4715cc8a9be17bedb2123da4ada5381404b53ca
Joaquin Nunez <jcbnunez@JCBN2020.local>
2023-04-12 16:31:10 -0400

    seas more

--- 11.Seasonality_Analysis/alanVersion/launch.seasonality.alan.sh
+++ 11.Seasonality_Analysis/alanVersion/launch.seasonality.alan.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 #
-#SBATCH -J glmAll # A single job name for the array
+#SBATCH -J glmAlLo # A single job name for the array
 #SBATCH -c 5
 #SBATCH -N 1 # on one node
 #SBATCH -t 12:00:00 #<= this may depend on your resources
 #SBATCH --mem 45G #<= this may depend on your resources
 #SBATCH -p standard
 #SBATCH -A berglandlab_standard
-#SBATCH -o ./slurmOut/glmAll.%A_%a.out # Standard output
-#SBATCH -e ./slurmOut/glmAll.%A_%a.err # Standard error
-#SBATCH --array=1-9060
+#SBATCH -o ./slurmOut/glmAlLo.%A_%a.out # Standard output
+#SBATCH -e ./slurmOut/glmAlLo.%A_%a.err # Standard error
+#SBATCH --array=1-1000
+
+###9060
 
 module load gcc/7.1.0 openmpi/3.1.4 R/4.1.1 gdal proj
 
@@ -17,7 +19,8 @@ Rscript \
 --vanilla \
 seasonality.R \
 ${SLURM_ARRAY_TASK_ID} \
-"all_seas"
+"all_seas" \
+"Phylo_LocRan"
 
 
 #Rscript \
--- 11.Seasonality_Analysis/alanVersion/seasonality.R
+++ 11.Seasonality_Analysis/alanVersion/seasonality.R
@@ -3,11 +3,13 @@
 
   args = commandArgs(trailingOnly=TRUE)
   jobId=as.numeric(args[1])
-  model=as.character(args[2])
-  #nJobs=as.numeric(args[2])
-
-  ## jobId=1; nJobs=5000
-
+  model=as.character(args[2]) # all_seas ; NoCore20_seas
+  model_features=as.character(args[3]) #No_Phylo; Phylo_LocRan
+  
+  #jobId=1
+  #model="all_seas"
+  #model_features="Phylo_LocRan"
+    
 ### libraries
   library(data.table)
   library(lubridate)
@@ -16,7 +18,8 @@
   library(doMC)
   registerDoMC(5)
   library(tidyverse)
-
+  library(lme4)
+  
   #setwd("/scratch/aob2x")
 
 ### load data
@@ -30,6 +33,8 @@
   
     core20.upd = left_join(core.20, samps[,c("sampleId", "sampleId_orig")])
 
+    seasonal.phylo.clusters = get(load("phylocluster_data.Rdata"))
+    
   #### model selector
     if(model == "all_seas") {
       
@@ -134,91 +139,112 @@
 
   tmp.ids <- snp.dt[chr==wins.i$chr][pos%in%c(wins.i$start:wins.i$end)]$variant.id
 
-  # tmp.ids <- c(1333833, 595225)
+  # tmp.ids <- c(759953, 1333833, 595225)
 
 ### iterate through
   o <- foreach(i=1:length(tmp.ids), .combine="rbind")%do%{
-    # i<-1
     message(paste(i, length(tmp.ids), sep=" / "))
     af <- getData(variant=tmp.ids[i])
     af <- merge(af, seasonal.sets, by="sampleId")
     af[,year_pop:=as.factor(interaction(locality, year))]
     af <- af[!is.na(af_nEff)]
-
-    # t1 <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ 1, data=af, family=binomial())
-    # t2 <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season, data=af, family=binomial())
-    #
-      t3.real <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ year_pop, data=af,
-                     family=quasibinomial())
-
-      t4.real <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season+year_pop, data=af,
-                     family=quasibinomial())
-
-    # t3.real <- glmer(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ 1 + (1|year_pop), data=af,
-    #            family=binomial())
-    # t4.real <- glmer(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season+ (1|year_pop), data=af,
-    #            family=binomial())
-
-
+    
+    left_join(af, seasonal.phylo.clusters) -> af
+    af$cluster = as.factor(af$cluster)
+    
+    #No_Phylo; Phylo_LocRan
+    if(model_features == "No_Phylo" ){
+      
+      message("No_Phylo model")
+      t3.real <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ year_pop,
+                     data=af, family=binomial())
+      t4.real <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season + year_pop,
+                     data=af, family=binomial())
+      
+      #lrt=anova(t3.real, t4.real, test="Chisq")$`Chisq`[2],
+      p_lrt=anova(t4.real, t3.real, test="Chisq")$`Pr(>Chi)`[2]
+      
+      
+    } else if(model_features == "Phylo_LocRan" ){
+      
+      message("Phylo_LocRan model")
+      t3.real <- glmer(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ cluster + (1 | year_pop),
+                       data = af, family = binomial)
+      t4.real <- glmer(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season + cluster + (1 | year_pop),
+                       data = af, family = binomial)
+      
+      p_lrt=anova(t4.real, t3.real, test="Chisq")$`Pr(>Chisq)`[2]
+      
+    }
+    
     obs <-
-    data.table(perm=0,
-               b_seas=coef(t4.real)[2], se_temp=summary(t4.real)$coef[2,2],
-               nTotal=dim(seasonal.sets)[1],
-               nObs=dim(af)[1],
-               nFixed=sum(af$af_nEff==0) + sum(af$af_nEff==1),
-               af=mean(af$af_nEff), neff=mean(af$nEff),
-               lrt=anova(t3.real, t4.real, test="Chisq")[2,4],
-               p_lrt=anova(t4.real, t3.real, test="Chisq")[2,5],
-               model=model
-               )
-
-
-
+      data.table(perm=0,
+                 b_seas=summary(t4.real)$coef[2,1], se_temp=summary(t4.real)$coef[2,2],
+                 nTotal=dim(seasonal.sets)[1],
+                 nObs=dim(af)[1],
+                 nFixed=sum(af$af_nEff==0) + sum(af$af_nEff==1),
+                 af=mean(af$af_nEff), neff=mean(af$nEff),
+                 #lrt=anova(t3.real, t4.real, test="Chisq")$`Chisq`[2],
+                 p_lrt=p_lrt,
+                 model=model,
+                 model_features=model_features
+      )
+    
+    
     set.seed(1234)
     nPerm <- 100
     perms <- foreach(j=1:nPerm, .combine="rbind")%dopar%{
-
+      
       tmp <- af
       tmp[,season:=sample(season)]
-
-      #t3.perm <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~        year_pop,
-      #               data=tmp, family=binomial())
-      #t4.perm <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season+year_pop,
-      #               data=tmp, family=binomial())
-
-      #t3.perm <- glmer(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ 1 + (1|year_pop),
-      #               data=tmp, family=binomial())
-      #t4.perm <- glmer(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season + (1|year_pop),
-      #               data=tmp, family=binomial())
-
-      t3.perm <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ year_pop,
-                     data=tmp, family=quasibinomial())
-      t4.perm <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season + year_pop,
-                     data=tmp, family=quasibinomial())
-
-
+  
+      #No_Phylo; Phylo_LocRan
+      if(model_features == "No_Phylo" ){
+        
+        #message("No_Phylo model")
+        t3.perm <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ year_pop,
+                       data=tmp, family=quasibinomial())
+        t4.perm <- glm(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season + year_pop,
+                       data=tmp, family=quasibinomial())
+        
+        p_lrt=anova(t4.perm, t3.perm, test="Chisq")$`Pr(>Chi)`[2]
+        
+      } else if(model_features == "Phylo_LocRan" ){
+        
+        #message("Phylo_LocRan model")
+        t3.perm <- glmer(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ cluster + (1 | year_pop),
+                         data = tmp, family = binomial)
+        t4.perm <- glmer(cbind(af_nEff*nEff, (1-af_nEff)*nEff) ~ season + cluster + (1 | year_pop),
+                         data = tmp, family = binomial)
+        
+        p_lrt=anova(t4.perm, t3.perm, test="Chisq")$`Pr(>Chisq)`[2]
+        
+        
+      }
+      
       data.table(perm=j,
-                 b_seas=coef(t4.perm)[2], se_temp=summary(t4.perm)$coef[2,2],
+                 b_seas=summary(t4.perm)$coef[2,1], se_temp=summary(t4.perm)$coef[2,2],
                  nTotal=dim(seasonal.sets)[1],
                  nObs=dim(tmp)[1],
                  nFixed=sum(tmp$af_nEff==0) + sum(tmp$af_nEff==1),
                  af=mean(tmp$af_nEff), neff=mean(tmp$nEff),
-                 lrt=anova(t3.perm, t4.perm, test="Chisq")[2,4],
-                 p_lrt=anova(t4.perm, t3.perm, test="Chisq")[2,5],
-                 model=model
+                 #lrt=anova(t3.perm, t4.perm, test="Chisq")$`Chisq`[2],
+                 p_lrt= p_lrt, #anova(t4.perm, t3.perm, test="Chisq")$`Pr(>Chisq)`[2],
+                 model=model,
+                 model_features=model_features
+                 
       )
-
+      
     }
-
+    
     out <- rbind(obs, perms)
     out[,variant.id:=tmp.ids[i]]
-
+    
   }
-
   o <- merge(o, snp.dt, by="variant.id")
-
+  
   #### SAVE O
-  output_file = "/scratch/yey2sn/DEST2_analysis/seasonality/GLM_ALAN_APR122023/"
+  output_file = "/scratch/yey2sn/DEST2_analysis/seasonality/GLM_newods_ALAN_APR122023/"
   save(o,
        file = paste(output_file,
                     "GLM_out.",
@@ -228,3 +254,25 @@
                     ".",
                     paste(wins.i$chr,wins.i$start,wins.i$end, sep = "_"),
                     ".Rdata", sep = ""))
+
+  ####
+  ####
+  ####
+  
+  ggplot() +
+    geom_violin(
+      data = o[perm != 0],
+      aes(
+        x=model_features,
+        y=-log10(p_lrt)
+      )) +
+    geom_point(
+      data = o[perm == 0],
+      aes(
+        x=model_features,
+        y=-log10(p_lrt)
+      )) +
+    facet_wrap(~variant.id, scales = "free")->
+    model.tester
+  ggsave(model.tester, file = "model.tester.pdf")
+  
