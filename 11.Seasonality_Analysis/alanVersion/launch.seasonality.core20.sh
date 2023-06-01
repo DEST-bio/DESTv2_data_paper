928494faf20effcf94e3cd881d5ac56fe70ec6bd
Alan Bergland <aob2x@virginia.edu>
2023-06-01 18:03:17 -0400

    Update launch.seasonality.core20.sh

--- 11.Seasonality_Analysis/alanVersion/launch.seasonality.core20.sh
+++ 11.Seasonality_Analysis/alanVersion/launch.seasonality.core20.sh
@@ -3,17 +3,24 @@
 #SBATCH -J glmOmn # A single job name for the array
 #SBATCH -c 5
 #SBATCH -N 1 # on one node
-#SBATCH -t 12:00:00 #<= this may depend on your resources
-#SBATCH --mem 45G #<= this may depend on your resources
+#SBATCH -t 00:60:00 #<= this may depend on your resources
+#SBATCH --mem 5G #<= this may depend on your resources
 #SBATCH -p standard
 #SBATCH -A berglandlab_standard
-#SBATCH -o ./slurmOut/glmOmn.%A_%a.out # Standard output
-#SBATCH -e ./slurmOut/glmOmn.%A_%a.err # Standard error
-#SBATCH --array=1-500
+#SBATCH -o /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.%A_%a.out # Standard output
+#SBATCH -e /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.%A_%a.err # Standard error
 
-###9060
+### sbatch --array=1000 /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion/launch.seasonality.core20.sh
+### sacct -j 49751805
+### cat /scratch/aob2x/Overwintering_18_19/ld_alan/logs/ld.49751619_17*.err
+### cat /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.49369837*.err
 
-module load gcc/7.1.0 openmpi/3.1.4 R/4.1.1 gdal proj
+
+
+module load gcc/7.1.0  openmpi/3.1.4 R/4.1.1
+
+cd /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion
+
 
 Rscript \
 --vanilla \
@@ -21,4 +28,3 @@ seasonality.R \
 ${SLURM_ARRAY_TASK_ID} \
 "Core20_seas" \
 100
-
