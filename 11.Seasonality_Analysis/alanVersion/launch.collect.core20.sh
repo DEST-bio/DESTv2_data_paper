6b102a3c5fd81893c41874ae94da85c649525772
Alan Bergland <aob2x@virginia.edu>
2023-06-05 09:11:04 -0400

    Update launch.collect.core20.sh

--- 11.Seasonality_Analysis/alanVersion/launch.collect.core20.sh
+++ 11.Seasonality_Analysis/alanVersion/launch.collect.core20.sh
@@ -5,14 +5,14 @@
 #SBATCH -N 1 # on one node
 #SBATCH -t 01:30:00 #<= this may depend on your resources
 #SBATCH --mem 100G #<= this may depend on your resources
-#SBATCH -p standard
+#SBATCH -p largemem
 #SBATCH -A berglandlab_standard
 #SBATCH -o /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.%A_%a.err # Standard error
 
-### sbatch /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion/launch.seasonality.core20.sh
+### sbatch /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion/launch.collect.core20.sh
 ###
-### sacct -j 50075916
+### sacct -j 50085745
 ### cat /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.50075916_199.err
 ### cat /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.49369837*.err
 
@@ -25,4 +25,4 @@ cd /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion
 
 Rscript \
 --vanilla \
-collect.seasonality.core20.R 
+collect.seasonality.core20.R
