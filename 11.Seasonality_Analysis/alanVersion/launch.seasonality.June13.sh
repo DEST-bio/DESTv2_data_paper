ae8d48c5f67052e57ad5a15b0450aa8a7231ca39
Alan Bergland <aob2x@virginia.edu>
2023-06-14 20:23:05 -0400

    Update launch.seasonality.June13.sh

--- 11.Seasonality_Analysis/alanVersion/launch.seasonality.June13.sh
+++ 11.Seasonality_Analysis/alanVersion/launch.seasonality.June13.sh
@@ -3,7 +3,7 @@
 #SBATCH -J glmOmn # A single job name for the array
 #SBATCH -c 5
 #SBATCH -N 1 # on one node
-#SBATCH -t 01:30:00 #<= this may depend on your resources
+#SBATCH -t 02:30:00 #<= this may depend on your resources
 #SBATCH --mem 15G #<= this may depend on your resources
 #SBATCH -p standard
 #SBATCH -A berglandlab_standard
@@ -14,8 +14,8 @@
 
 ### sbatch --array=1-2173 /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion/launch.seasonality.June13.sh
 ###
-### sacct -j 50618906
-### cat /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.50618748_450.err
+### sacct -j 50623725
+### cat /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.50623725_100.err
 ### cat /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.49369837*.err
 ### cd /scratch/aob2x/DEST2_analysis/seasonality/logs/; perl -e 'for(<*>){unlink}'; cd -
 
