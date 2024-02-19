ea93c893550c044b33e44c6a397162d3c8d57488
Alan Bergland <aob2x@virginia.edu>
2024-02-19 11:10:52 -0500

    Update launch.windowAnalysis_glm_perm.sh

--- 11.Seasonality_Analysis/alanVersion/windowEnrichmentTests/launch.windowAnalysis_glm_perm.sh
+++ 11.Seasonality_Analysis/alanVersion/windowEnrichmentTests/launch.windowAnalysis_glm_perm.sh
@@ -10,22 +10,15 @@
 #SBATCH -o /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.%A_%a.err # Standard error
 
-### sbatch --array=1-101 /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion/launch.seasonality.Europe_29_sept_2023.sh
+### sbatch --array=1-101 /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion/windowEnrichmentTests/launch.windowAnalysis_glm_perm.sh
 ### sacct -j 53885553 --format="JobID%30,JobName,ExitCode,State" | grep "TIMEOUT" | sed -e 's/\s\+/,/g' | cut -f2 -d',' | cut -f2 -d'_' | tr '\n' ','
 ### seff 50160643_1
 ### cat /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.50142438_1000.out
 ### cat /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.53669286_1.err
 
-
-
-module load gcc/7.1.0  openmpi/3.1.4 R/4.1.1
-
-cd /home/aob2x/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion
-
+module module load gcc/11.4.0 openmpi/4.1.4 R/4.3.1
 
 Rscript \
 --vanilla \
-seasonality_v2.R \
-${SLURM_ARRAY_TASK_ID} \
-"NoCore20_seas_europe" \
-100
+~/DESTv2_data_paper/11.Seasonality_Analysis/alanVersion/windowEnrichmentTests/windowAnalysis_glm_perm.R \
+${SLURM_ARRAY_TASK_ID}
