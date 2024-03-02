2f49b7d6c8c0f7b89418bd0a762a4814ee41ec7c
Alan Bergland <aob2x@virginia.edu>
2024-03-02 09:57:13 -0500

    Update 3.1.launch_lf3stat_AOB.sh

--- 10.Admixture_f3/3.1.launch_lf3stat_AOB.sh
+++ 10.Admixture_f3/3.1.launch_lf3stat_AOB.sh
@@ -3,17 +3,17 @@
 #SBATCH -J glmOmn # A single job name for the array
 #SBATCH -c 7
 #SBATCH -N 1 # on one node
-#SBATCH -t 00:30:00 #<= this may depend on your resources
+#SBATCH -t 00:60:00 #<= this may depend on your resources
 #SBATCH --mem 63G #<= this may depend on your resources
 #SBATCH -p standard
 #SBATCH -A berglandlab_standard
 #SBATCH -o /scratch/aob2x/DEST2_analysis/seasonality/logs/f3.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/DEST2_analysis/seasonality/logs/f3.%A_%a.err # Standard error
 
-### sbatch --array=1-4912 /home/aob2x/DESTv2_data_paper/DESTv2_data_paper/10.Admixture_f3/3.0.calculate.f3stats_AOB.sh
-### sacct -j 58265536
+### sbatch --array=1-4912 /home/aob2x/DESTv2_data_paper/10.Admixture_f3/3.1.launch_lf3stat_AOB.sh
+### sacct -j 59036995
 ### cat /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.58265536*.err
-### cat /scratch/aob2x/DEST2_analysis/seasonality/logs/glmOmn.53669286_1.err
+### cat /scratch/aob2x/DEST2_analysis/seasonality/logs/f3.59036995_174.err
 
 
 module load gcc/7.1.0  openmpi/3.1.4 R/4.1.1
