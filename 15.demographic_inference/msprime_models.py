66ffa6f52cc12aac03d0eeff32af4147fe4f0fe4
David <davidbass256@gmail.com>
2024-07-09 10:16:46 -0400

    draft demesdraw figs

--- 15.demographic_inference/msprime_models.py
+++ 15.demographic_inference/msprime_models.py
@@ -12,8 +12,8 @@ def get_split_dem(n_anc, params):
     dem = msprime.Demography()
     dem.add_population(name="A", initial_size=n_anc*nu1)  # pop1 at present time
     dem.add_population(name="B", initial_size=n_anc*nu2)  # pop2 at present time
-    dem.add_population(name="C", initial_size=n_anc)  # ancestral pop
-    dem.add_population_split(time=2*n_anc*T, derived=["A", "B"], ancestral="C")
+    dem.add_population(name="ancestral", initial_size=n_anc)  # ancestral pop
+    dem.add_population_split(time=2*n_anc*T, derived=["A", "B"], ancestral="ancestral")
     dem.set_symmetric_migration_rate(["A", "B"], m/(2*n_anc))
     return dem
 
@@ -33,15 +33,24 @@ def get_split_asymmig_dem(n_anc, params):
 def get_admixture_dem(n_anc, params):
     # It is necessary that T_split > T_admix because the split from the ancestral 
     # precedes the admixture event.
-    nu1, nu2, nu_admix, T_split, T_admix, p_admix = params
+    nu1, nu2, nu_admix, T_split, T_admix, m2, m3, p_admix = params
+
+    # Initialize populations in `Demography` object
     dem = msprime.Demography()
     dem.add_population(name="ancestral", initial_size=n_anc)
     dem.add_population(name="A", initial_size=n_anc*nu1)
     dem.add_population(name="B", initial_size=n_anc*nu2)
     dem.add_population(name="admixed", initial_size=n_anc*nu_admix)
+
+    # Admixture of two derived populations
     dem.add_admixture(derived="admixed", ancestral=["A", "B"],
     				  time=2*n_anc*T_admix, proportions=[p_admix, 1 - p_admix])
+    dem.set_symmetric_migration_rate(["A", "B", "admixed"], m3/(2*n_anc))
+
+    # Split of two derived populations from ancestral population
     dem.add_population_split(time=2*n_anc*T_split, derived=["A", "B"], ancestral="ancestral")
+    dem.set_symmetric_migration_rate(["A", "B"], m2/(2*n_anc))
+
     return dem
 
 def get_two_splits_dem(n_anc, params):
---
+++ 15.demographic_inference/viz_dem_models_with_demesdraw.py
@@ -0,0 +1,25 @@
+import msprime
+import demesdraw
+import matplotlib.pyplot as plt
+import msprime_models as msm
+
+def main():
+    dems = {"one-population": msm.get_one_population_dem(1e4, [3, 2]),
+            "split": msm.get_split_dem(1e4, [3, 2, 1, 1]),
+            "split-asymmig": msm.get_split_asymmig_dem(1e4, [3, 2, 1, 1, 0.5]),
+            "admixture": msm.get_admixture_dem(1e4, [3, 2, 1, 2, 1, 0, 0, 0.1]),
+            "two-splits": msm.get_two_splits_dem(1e4, [3, 2, 1, 2, 1, 0.5, 1, 0])}
+    output_dir = "draft_figures/"
+
+    viz_dem_models_with_demesdraw(dems, output_dir)
+
+def viz_dem_models_with_demesdraw(dems, output_dir):
+    for name, dem in dems.items():
+        print(name)
+        graph = msprime.Demography.to_demes(dem)
+        fig, ax = plt.subplots()
+        demesdraw.tubes(graph, ax=ax, seed=1)
+        plt.savefig(f"{output_dir}{name}_demesdraw.png")
+
+if __name__ == "__main__":
+    main()
