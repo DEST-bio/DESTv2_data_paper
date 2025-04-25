5fa81c04b4b73aba7229467cbad8109d8edd6506
jcnunez <15324935+Jcbnunez@users.noreply.github.com>
2025-04-25 08:06:54 -0400

    apr25.2025

--- 15.demographic_inf_plus_admix/MBE.Revision3.ADMIXTURE/5.plot_analyze.R
+++ 15.demographic_inf_plus_admix/MBE.Revision3.ADMIXTURE/5.plot_analyze.R
@@ -7,21 +7,22 @@ library(poolfstat)
 library(FactoMineR)
 require(foreach)
 require(gmodels)
+require(DescTools)
 
 meta <- fread("https://raw.githubusercontent.com/DEST-bio/DESTv2/refs/heads/main/populationInfo/dest_v2.samps_24Aug2024.xa.csv")
 
-files <- system(paste("ls moments_output_NoMigration" ), intern = T)
+files <- system(paste("ls France_Parent/moments_output_NoMigration" ), intern = T)
 
 inf <- foreach(i=files, .combine = "rbind", .errorhandling = "remove")%do%{
-  tmp <- fread(paste("moments_output_NoMigration/",i, sep = ""))
+  tmp <- fread(paste("France_Parent/moments_output_NoMigration/",i, sep = ""))
 }
 inf %<>% 
   mutate(model = "noMig")
 
-files2 <- system(paste("ls moments_output_wMigration" ), intern = T)
+files2 <- system(paste("ls France_Parent/moments_output_wMigration" ), intern = T)
 
 inf2 <- foreach(i=files2, .combine = "rbind", .errorhandling = "remove")%do%{
-  tmp <- fread(paste("moments_output_wMigration/",i, sep = ""))
+  tmp <- fread(paste("France_Parent/moments_output_wMigration/",i, sep = ""))
 }
 inf2 %<>% 
   mutate(model = "Mig")
@@ -97,7 +98,43 @@ ggsave(corr_methods, file = "corr_methods.pdf",
 save(o.j, file = "ancestry_Estimates_linear_moments.Rdata")
 
 
+##### Ukraine model
+files3 <- system(paste("ls *_output.admix.txt" ), intern = T)
 
-  
-  
-  
+inf3 <- foreach(i=files3, .combine = "rbind", .errorhandling = "remove")%do%{
+  tmp <- fread(paste("./",i, sep = ""))
+}
+inf3 %<>% 
+  mutate(model = "UApar")
+
+inf3 %>%
+  mutate(sampleId = V1) %>%
+  group_by(sampleId, model) %>%
+  summarise(ancestry = ci(admix_prop)[1],
+            uci = ci(admix_prop)[2],
+            lci = ci(admix_prop)[3]) %>%
+  left_join(meta) ->
+  o.UA
+
+o.NA <- get(load("MOMENTS.AdmixtureProp.EuropeanProb.Rdata"))
+
+left_join(select(o.UA, sampleId, UA_ancestry=ancestry), 
+          select(o.NA, sampleId, FR_ancestry=ancestry)) %>%
+  .[complete.cases(.),]->
+  anc.joint.UA_FR
+
+cor.test(~UA_ancestry+FR_ancestry, data = anc.joint.UA_FR)
+CCC(anc.joint.UA_FR$UA_ancestry, anc.joint.UA_FR$FR_ancestry)
+
+anc.joint.UA_FR %>%
+  ggplot(aes(
+    x=1-UA_ancestry,
+    y=1-FR_ancestry,
+  )) + 
+  geom_point() +
+  #geom_smooth(method = "lm") +
+  geom_abline(slope = 1) +
+  theme_bw() -> momemts_UA_FR
+
+ggsave(momemts_UA_FR, file = "momemts_UA_FR.pdf",
+       w= 4, h = 4)
